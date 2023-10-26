62a299df1b9146406eaaadd9c7409b08f662240b
Code With_Dipak <140307138+Code-With-Dipak@users.noreply.github.com>
2023-10-26 15:23:46 +0530

    template.cpp
         template

---
+++ template.cpp
@@ -0,0 +1,36 @@
+ // Template //
+#include <iostream>
+using namespace std;
+
+template <class t> // Template 1 //
+t sum(t a,t b)
+{
+	cout<<"\n addition is:-"<<a+b;
+	
+}
+
+template <class p> // Template 2//
+p sub(p a,p b)
+{
+	cout<<"\n subtraction is:-"<<a-b;
+	
+}
+
+int main()
+{
+	sum(100,100);
+	sub(300,200);
+	return 0;
+	
+}
+
+/*
+OUTPUT:-
+
+	comp@comp-HP-EliteDesk-800-G2-SFF:~$ gedit Dipak.cpp
+	comp@comp-HP-EliteDesk-800-G2-SFF:~$ g++ Dipak.cpp
+	comp@comp-HP-EliteDesk-800-G2-SFF:~$ ./a.out
+
+		 addition is:-200
+		 subtraction is:-100
+*/
